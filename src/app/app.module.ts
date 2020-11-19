822bb70d2fa062388c7df907ebe72860db95abeb
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2020-11-19 21:17:59 +0100

    Update: correct syntax errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessageComponent } from './message/message.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessageComponent
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
