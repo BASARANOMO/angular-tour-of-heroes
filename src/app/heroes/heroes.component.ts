458b0668509a75d6603557f12cf201f8a53aebc7
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2020-11-19 10:46:40 +0100

    Update: add click event handler onSelect()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,17 +10,17 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   //hero = 'Windstorm';
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  
   heroes = HEROES;
+
+  selectedHero: Hero | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 }
